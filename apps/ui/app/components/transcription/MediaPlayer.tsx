b7a6f42ba58b9d27ea1f4c68ca08b12fd0873604
Nicolas Morel <nmr.morel@gmail.com>
2023-04-23 09:19:59 +0200

    Improvement

--- apps/ui/app/components/transcription/MediaPlayer.tsx
+++ apps/ui/app/components/transcription/MediaPlayer.tsx
@@ -1,15 +1,32 @@
-import {forwardRef} from 'react'
+import {RefCallback} from 'react'
+import {MutableRefObject} from 'react'
+import {Ref} from 'react'
+import {LegacyRef, forwardRef} from 'react'
 
-export const MediaPlayer = forwardRef(function MediaPlayer({
-  kind,
-  src,
-}: {
-  kind: 'audio' | 'video'
-  src: string
-}) {
+export const MediaPlayer = forwardRef(function MediaPlayer(
+  {
+    kind,
+    src,
+  }: {
+    kind: 'audio' | 'video'
+    src: string
+  },
+  _ref:
+    | RefCallback<HTMLAudioElement | HTMLVideoElement | null>
+    | MutableRefObject<HTMLAudioElement | HTMLVideoElement | null>
+    | null
+) {
+  // To please TS, otherwise the video ref is complaining
+  const ref = (elt: HTMLAudioElement | HTMLVideoElement | null) => {
+    if (typeof _ref === 'function') {
+      _ref(elt)
+    } else if (_ref) {
+      _ref.current = elt
+    }
+  }
   return kind === 'audio' ? (
-    <audio src={src} controls className="w-full" />
+    <audio ref={ref} src={src} controls className="w-full" />
   ) : (
-    <video src={src} controls className="w-full" />
+    <video ref={ref} src={src} controls className="w-full" />
   )
 })
